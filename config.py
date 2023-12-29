91522d6e8768858db4a6871a68497e493bc6c0e2
hainlh <longhai230303@gmail.com>
2023-12-29 20:18:56 +0700

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ CONFIG = {
     'gpu_id': "0",
     'note': 'some_note',
     'model': 'MIDGN',
-    'dataset_name': 'Steam',
+    'dataset_name': 'iFashion',
     'task': 'tune',
     'eval_task': 'test',
 
@@ -22,17 +22,17 @@ CONFIG = {
     'sample': 'simple',
     'hard_window': [0.7, 1.0], # top 30%
     'hard_prob': [0.3, 0.3], # probability 0.8
-    'conti_train': 'log/Steam/',
+    'conti_train': 'log/iFashion/',
 
     ## other settings
-    'epochs': 200,
+    'epochs': 100,
     'early': 50,
     'log_interval': 5,
     'test_interval': 5,
     'retry': 1,
 
     ## test path
-    'test':['log/Steam'],
+    'test':['log/iFashion'],
     'batch_size_train': 2048,
     'batch_size_test': 4096,
     'n_layers': 2,
